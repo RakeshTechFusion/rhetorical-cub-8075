bf7769362c0e738611e830e903869c5747545657
Adarsh M. Khandare <kingadarsh313@gmail.com>
2022-08-28 14:40:23 +0530

    some changes in cart

--- frontend/src/Pages/cart/Singlecartprod.jsx
+++ frontend/src/Pages/cart/Singlecartprod.jsx
@@ -1,103 +1,191 @@
-import React,{ useEffect } from "react";
-import { Box, VStack, Flex , Image,Text ,Button   } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { Box, VStack, Flex, Image, Text, Button } from "@chakra-ui/react";
 import { MdDelete } from "react-icons/md";
 import { IoIosArrowBack } from "react-icons/io";
 import { useDispatch, useSelector } from "react-redux";
-import axios from "axios"
-import {totalpricee} from "../../redux/cartReducer"
+import axios from "axios";
+import { totalpricee } from "../../redux/cartReducer";
 
+export const SingleProduct = ({
+  setTotalprice,
+  totalprice,
+  elm,
+  setCartProds,
+}) => {
+  const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const [price, setprice] = React.useState(0);
+  const [prod, setprod] = React.useState({});
+  const [qty, setqty] = React.useState(elm.quantity);
 
-export const SingleProduct=({setTotalprice,totalprice,elm,setCartProds})=>{
-    const { currentUser } = useSelector((state) => state.user);
-    const dispatch = useDispatch();
-    const [price,setprice] = React.useState(0)
-    const [prod,setprod] = React.useState({
-        
-    })
-    const [qty,setqty] = React.useState(elm.quantity)
-    useEffect(() => {
-        axios.get(`http://localhost:8080/api/products/${elm.productId}`)
-        .then((res)=>{
-            setprod({...res.data})
-            setprice(res.data.Price)
-            setTotalprice((state) => state + (res.data.Price * elm.quantity))
-            dispatch(totalpricee(totalprice))
-            setqty(elm.quantity)
-        })
-      }, [])
+  const getdata = () => {
+    axios
+      .get(`http://localhost:8080/api/cart/${currentUser._id}`)
+      .then((res) => {
+        console.log("zzzzzzzzz", res.data, "****", currentUser._id);
+        setCartProds(res.data);
+      });
+  };
+  useEffect(() => {
+    const getProducts = () => {
+      axios
+        .get(`http://localhost:8080/api/products/${elm.productId}`)
+        .then((res) => {
+          setprod({ ...res.data });
+          setprice(res.data.Price);
+          setTotalprice((state) => state + res.data.Price * elm.quantity);
+          dispatch(totalpricee(totalprice));
+          setqty(elm.quantity);
+        });
+    };
+    getProducts();
+  }, []);
 
-      const deleteprod=()=>{
-            axios.delete(`http://localhost:8080/api/cart/${elm._id}`)
-            .then((res)=>{
-                setTotalprice((state) => state - (price * qty));
-                // setCartProds((state) => state.filter((elm) => elm._id !== elm._id))
-                console.log(res.data)
-            })
-            .catch((err)=> console.log(err))
-      }
-    
-      const handleqty = (act)=>{
-            if(act=="dec" && qty==1){
-                return alert("qty cannot be zero")
-            }
+  const deleteprod = () => {
+    axios
+      .delete(`http://localhost:8080/api/cart/${elm._id}`)
+      .then((res) => {
+        setTotalprice((state) => state - price * qty);
+        setCartProds((state) => state.filter((elm) => elm._id !== elm._id))
+        getdata();
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
 
-            axios.put(`http://localhost:8080/api/cart/${act}/${elm._id}`)
-            .then((res)=>{
-                if(act=="inc"){
-                    setTotalprice((state) => state + price)
-                    setqty((state)=>state+1)
-                }
-                else{
-                    setTotalprice((state) => state - price)
-                    setqty((state)=>state-1)
-                }
-                dispatch(totalpricee(totalprice))
-            })
-      }
+  const handleqty = (act) => {
+    if (act == "dec" && qty == 1) {
+      return alert("qty cannot be zero");
+    }
 
-    return( 
-            <Flex mt="0.5rem"  w="100%" backgroundColor="#fff">
-                    <Box backgroundColor="#fff" h="110px" w="100%">
-                        <Flex p="0.5rem" w="100%"> 
-                            <Flex h="100px" w="60%" >
-                                <Box w="25%"  p="5px 15px">
-                                    <Image h="100%" w="100%" borderRadius="0.25rem" src={prod.ImageUrl} alt=''/>
-                                </Box>
-                                <Box w="70%">
-                                    <Box fontSize="12px" fontWeight="550" color="#575555" textAlign="left" ml="5px" mt="5px">
-                                        <Text mb="2px">{prod.Title}</Text>
-                                    </Box>
-                                    <Flex ml="5px" textAlign="left" fontSize="10px" fontWeight="500" color="#575555" gap="5px">
-                                        <Box>
-                                        <Image h="15px" w="15px" borderRadius="0.25rem" src='https://in.sugarcosmetics.com/desc-images/Wishlist.svg' alt=''/>
-                                        </Box>
-                                        <Box>Save to Wishlist</Box>
-                                    </Flex>
-                                </Box>
-                            </Flex>
-                            <Box w="40%" textAlign="right" mt="1.5rem" >               
-                                <Flex fontSize="12px"  gap="20px">
-                                        <Box  w="1em" h="1em" color="black" fontSize="16px" verticalAlign="middle"><MdDelete onClick={()=>deleteprod()} /></Box>
-                                        <Flex justifyContent="space-around" gap="5px">
-                                            <Box position="relative" borderRadius="50%" w="19px" h="19px" fontSize="18px" backgroundColor="black" color="white" onClick={()=>handleqty("dec")} >
-                                                <Box fontSize="13px" w="100%" h="100%" position="absolute" pb="10px" pr="7px">-</Box>
-                                            </Box>
-                                            <Box>{qty}</Box>
-                                            <Box position="relative" borderRadius="50%" w="19px" h="19px" fontSize="18px" backgroundColor="black" color="white" onClick={()=>handleqty("inc")} >
-                                                <Box fontSize="13px" w="100%" h="100%" position="absolute" pb="7px" pr="5px">+</Box>
-                                            </Box>
-                                        </Flex>
-                                        <Flex>
-                                            <Text textDecoration="underline" fontWeight="400" >{qty}</Text>
-                                            <Text>×</Text>
-                                            <Text textDecoration="line-through" >{price}=</Text>
-                                            <Text color="#fc2779" >₹{qty*price}</Text>
-                                        </Flex>
-                                </Flex>
-                            </Box>
-                        </Flex>
-                    </Box>
-                </Flex>
-        
-    )
-}
+    axios
+      .put(`http://localhost:8080/api/cart/${act}/${elm._id}`)
+      .then((res) => {
+        if (act == "inc") {
+          setTotalprice((state) => state + price);
+          setqty((state) => state + 1);
+        } else {
+          setTotalprice((state) => state - price);
+          setqty((state) => state - 1);
+        }
+        dispatch(totalpricee(totalprice));
+      });
+  };
+
+  return (
+    <Flex mt="0.5rem" w="100%" backgroundColor="#fff">
+      <Box backgroundColor="#fff" h="110px" w="100%">
+        <Flex p="0.5rem" w="100%">
+          <Flex h="100px" w="60%">
+            <Box w="25%" p="5px 15px">
+              <Image
+                h="100%"
+                w="100%"
+                borderRadius="0.25rem"
+                src={prod.ImageUrl}
+                alt=""
+              />
+            </Box>
+            <Box w="70%">
+              <Box
+                fontSize="12px"
+                fontWeight="550"
+                color="#575555"
+                textAlign="left"
+                ml="5px"
+                mt="5px"
+              >
+                <Text mb="2px">{prod.Title}</Text>
+              </Box>
+              <Flex
+                ml="5px"
+                textAlign="left"
+                fontSize="10px"
+                fontWeight="500"
+                color="#575555"
+                gap="5px"
+              >
+                <Box>
+                  <Image
+                    h="15px"
+                    w="15px"
+                    borderRadius="0.25rem"
+                    src="https://in.sugarcosmetics.com/desc-images/Wishlist.svg"
+                    alt=""
+                  />
+                </Box>
+                <Box>Save to Wishlist</Box>
+              </Flex>
+            </Box>
+          </Flex>
+          <Box w="40%" textAlign="right" mt="1.5rem">
+            <Flex fontSize="12px" gap="20px">
+              <Box
+                w="1em"
+                h="1em"
+                color="black"
+                fontSize="16px"
+                verticalAlign="middle"
+              >
+                <MdDelete onClick={() => deleteprod()} />
+              </Box>
+              <Flex justifyContent="space-around" gap="5px">
+                <Box
+                  position="relative"
+                  borderRadius="50%"
+                  w="19px"
+                  h="19px"
+                  fontSize="18px"
+                  backgroundColor="black"
+                  color="white"
+                  onClick={() => handleqty("dec")}
+                >
+                  <Box
+                    fontSize="13px"
+                    w="100%"
+                    h="100%"
+                    position="absolute"
+                    pb="10px"
+                    pr="7px"
+                  >
+                    -
+                  </Box>
+                </Box>
+                <Box>{qty}</Box>
+                <Box
+                  position="relative"
+                  borderRadius="50%"
+                  w="19px"
+                  h="19px"
+                  fontSize="18px"
+                  backgroundColor="black"
+                  color="white"
+                  onClick={() => handleqty("inc")}
+                >
+                  <Box
+                    fontSize="13px"
+                    w="100%"
+                    h="100%"
+                    position="absolute"
+                    pb="7px"
+                    pr="5px"
+                  >
+                    +
+                  </Box>
+                </Box>
+              </Flex>
+              <Flex>
+                <Text textDecoration="underline" fontWeight="400">
+                  {qty}
+                </Text>
+                <Text>×</Text>
+                <Text textDecoration="line-through">{price}=</Text>
+                <Text color="#fc2779">₹{qty * price}</Text>
+              </Flex>
+            </Flex>
+          </Box>
+        </Flex>
+      </Box>
+    </Flex>
+  );
+};
